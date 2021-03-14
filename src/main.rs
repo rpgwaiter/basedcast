428693bf0060b12cd6ee517e180d7c8d7864e0f9
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-14 14:58:09 -0500

    added function paren

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use mpd::Client;
 use std::net::TcpStream;
 use std::env;
 
-fn main {
+fn main() {
     let mut conn = Client::connect("127.0.0.1:6600").unwrap(); // use env vars eventually print_env_var("");
     conn.volume(100).unwrap();
     conn.load("My Lounge Playlist", ..).unwrap();
