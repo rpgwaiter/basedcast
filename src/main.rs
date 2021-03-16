42498bbc2d1aa63369e3337d34d2d0d2d3106437
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-16 12:53:07 -0500

    commended macros

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 extern crate mpd;
+// #[macro_use]
+extern crate diesel;
+// #[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
 
 use mpd::Client;
 use std::net::TcpStream;
 use std::env;
 
 fn main() {
-    let mut conn = Client::connect(format!("{}:{}", env::var(MPD_URL), env::var(MPD_PORT) )).unwrap();
+    let mpd_url = match env::var("MPD_URL") {
+        Ok(val) => val,
+        Err(e) => panic!("could not find {}: {}", MPD_URL, e),
+    };
+
+    let mut conn = Client::connect(println!("{}:{}", 
+        //env::var(MPD_URL), 
+        "127.0.0.1",
+        //env::var(MPD_PORT) 
+        "6600"
+    )).unwrap();
     conn.volume(100).unwrap();
     conn.load("My Lounge Playlist", ..).unwrap();
     conn.play().unwrap();
