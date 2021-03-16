51d43c3dafeac166ce85a0230ca293c56617f020
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-16 14:01:35 -0500

    added ld path to shell

--- shell.nix
+++ shell.nix
@@ -11,7 +11,8 @@ in
     ];
 
     shellHook = ''export CFG_DISABLE_CROSS_TESTS=1'';
-
+    
+    LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
     MPD_URL = "127.0.0.1";
     MPD_PORT = "6600";
   }
