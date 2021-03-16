12689282a8163f57d745a78df762a435d3609173
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-16 13:28:06 -0500

    use envmnt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 extern crate mpd;
-#[macro_use]
+//#[macro_use]
 extern crate diesel;
 
-use mpd::Client;
-use std::net::TcpStream;
+// use mpd::Client;
+//use std::net::TcpStream;
 use std::env;
+use envmnt::{ExpandOptions, ExpansionType};
+// use std::net::{SocketAddr, ToSocketAddrs};
+
+//static MPD_ADDR: &'static str = println!("{}:{}", env::var("MPD_URL"), env::var("MPD_PORT"));
 
 fn main() {
-    let mpd_url = match env::var("MPD_URL") {
-        Ok(val) => val,
-        Err(e) => panic!("could not find {}: {}", MPD_URL, e),
-    };
 
-    let mut conn = Client::connect(println!("{}:{}", 
-        //env::var(MPD_URL), 
-        "127.0.0.1",
-        //env::var(MPD_PORT) 
-        "6600"
-    )).unwrap();
-    conn.volume(100).unwrap();
-    conn.load("My Lounge Playlist", ..).unwrap();
-    conn.play().unwrap();
-    println!("Status: {:?}", conn.status());
+    // let mut conn = Client::connect(&MPD_ADDR).unwrap();
+    // conn.volume(100).unwrap();
+    // conn.load("My Lounge Playlist", ..).unwrap();
+    // conn.play().unwrap();
+    // println!("Status: {:?}", conn.status());
+    println!("{:?}", envmnt::get_or("MPD_URL", "DEFAULT_VALUE"))
 }
