39d9b4f08a037bbaad4dabc137d65d5393bd2c04
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-16 11:52:12 -0500

    fix syntax

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::net::TcpStream;
 use std::env;
 
 fn main() {
-    let mut conn = Client::connect(format!("{}:{}", env::var(MPD_URL, MPD_PORT))).unwrap();
+    let mut conn = Client::connect(format!("{}:{}", env::var(MPD_URL), env::var(MPD_PORT) )).unwrap();
     conn.volume(100).unwrap();
     conn.load("My Lounge Playlist", ..).unwrap();
     conn.play().unwrap();
