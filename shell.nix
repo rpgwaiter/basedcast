8692d2d0d4a087d141c8cf91f0b4fc4110190e1f
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-16 15:06:52 -0500

    What's the issue here

--- shell.nix
+++ shell.nix
@@ -1,15 +1,9 @@
 # Latest Nightly
 with import <nixpkgs> {};
-let src = fetchFromGitHub {
-      owner = "mozilla";
-      repo = "nixpkgs-mozilla";
-      # commit from: 2019-05-15
-      rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
-      sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
-   };
+let src = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
 in
 with import "${src.out}/rust-overlay.nix" pkgs pkgs;
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "rust-env";
   buildInputs = [
     # Note: to use stable, just replace `nightly` with `stable`
