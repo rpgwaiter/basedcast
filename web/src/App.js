ccb4a1ab58ae10fa1c7500d6ed9d5975729fb316
rpgwaiter <rpgwaiter@based.zone>
2021-05-12 01:58:54 -0500

    We got an app!

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Button, GlobalStyle, ThemeProvider } from '@react95/core';
+import { Button, GlobalStyle, ThemeProvider, Video } from '@react95/core';
 import styled from 'styled-components';
 
 import logo from './windows95_logo.png';
+import bg from './bg.gif';
 
 const Centered = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: url(${bg});
+  background-size: cover;
+
   height: 100%;
 `;
 
@@ -17,8 +21,9 @@ function App() {
     <ThemeProvider>
       <GlobalStyle></GlobalStyle>
       <Centered>
-        <img src={logo} style={{ width: 200 }} />
-        <Button>React95 Example</Button>
+        <img alt="logo" src={logo} style={{ width: 200 }} />
+        <Video src="https://cast.based.zone"></Video>
+        <Button>Based</Button>
       </Centered>
     </ThemeProvider>
   );
