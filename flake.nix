61a49ab188b9bb47a6f13d6e12d5a7e5be1bde52
rpgwaiter <rpgwaiter@based.zone>
2021-05-11 22:15:05 -0500

    added yarn pkgs

--- flake.nix
+++ flake.nix
@@ -5,24 +5,32 @@
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    yarn2nix = { url = "github:nix-community/yarn2nix"; flake = false; };
   };
 
   outputs = inputs:
     let
       outputs = inputs.nixCargoIntegration.lib.makeOutputs {
         root = ./.;
-        # buildPlatform = "crate2nix";
-        # overrides = with inputs.nixpkgs.legacyPackages.x86_64-linux; {
-        #   common = prev: {
-        #     buildInputs = prev.buildInputs ++ [ zlib openssl wasm-pack ];
+        buildPlatform = "crate2nix";
+        overrides = with pkgs; {
+          common = prev: {
+            buildInputs = prev.buildInputs ++ [ zlib openssl wasm-pack ];
 
-        #     nativeBuildInputs = prev.nativeBuildInputs ++ [ zlib ];
+            nativeBuildInputs = prev.nativeBuildInputs ++ [ zlib ];
 
-        #     runtimeLibs = prev.runtimeLibs ++ [ zlib ];
-        #   };
-        # };
+            runtimeLibs = prev.runtimeLibs ++ [ zlib ];
+          };
+        };
       };
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux // rec {
+        nodePackages.create-react-app = inputs.nixpkgs.legacyPackages.x86_64-linux.nodePackages.create-react-app.override {
+          preRebuild = ''
+            substituteInPlace $(find -type f -name createReactApp.js) \
+                --replace "path.join(root, 'yarn.lock')" "path.join(root, 'yarn.lock')); fs.chmodSync(path.join(root, 'yarn.lock'), 0o644"
+          '';
+        };
+      };
       mkContainer = drv:
         pkgs.dockerTools.buildImage rec {
           name = "${drv.name}";
@@ -30,19 +38,25 @@
             Cmd = [ "${drv}/bin/${name}" ];
           };
         };
-    in
+      y2n = (import "${inputs.yarn2nix}/default.nix" { pkgs = pkgs; });
+    in with y2n;
     pkgs.lib.recursiveUpdate
     outputs {
-      packages.x86_64-linux = pkgs.lib.mapAttrs' (name: drv: {
+      packages.x86_64-linux = {
+        basedcast_web = pkgs.yarn2nix-moretea.mkYarnPackage {
+            name = "basedcast_web";
+            src = ./web;
+            packageJSON = ./web/package.json;
+            yarnLock = ./web/yarn.lock;
+            yarnNix = ./web/yarn.nix;
+          };
+
+      } // pkgs.lib.mapAttrs' (name: drv: {
         name = "${name}-container";
         value = mkContainer drv;
       }) outputs.packages.x86_64-linux;
-      devenv = pkgs.dockerTools.buildImage rec {
-          name = "devenv";
-          # config = {
-          #   Cmd = [ "" ];
-          # };
-        };
+
+
     };
       #mapAttrs' mkContainer outputs.packages
 }
